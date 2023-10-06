f09be93e4b3d4059c911243e823adbae0c5ce119
ruinary <re.meral@mail.ru>
2023-10-06 17:15:04 +0300

    Initial commit

---
+++ MekashronUmbracoNetProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MekashronUmbracoNetProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
